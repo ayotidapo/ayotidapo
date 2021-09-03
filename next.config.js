a8326b63451a86bc9485f40a9beac39f8690cc33
otidapson <oladapo.o@softcom.ng>
2021-09-03 12:48:27 +0100

    remove env var value

--- next.config.js
+++ next.config.js
@@ -4,16 +4,16 @@ module.exports = (phase) => {
   let env = {};
   if (phase === PHASE_DEVELOPMENT_SERVER) {
     env = {
-      mongodb_username: "ayotidapo",
-      mongodb_password: "9kRH3v3NxNN43V2o",
-      mongodb_clustername: "cluster0",
+      mongodb_username: "",
+      mongodb_password: "",
+      mongodb_clustername: "",
       mongodb_database: "my-portfolio",
     };
   } else {
     env = {
-      mongodb_username: "ayotidapo_prod",
-      mongodb_password: "2YkcXq43KyPk0vqp",
-      mongodb_clustername: "cluster0",
+      mongodb_username: "",
+      mongodb_password: "",
+      mongodb_clustername: "",
       mongodb_database: "my-portfolio-prod",
     };
   }
