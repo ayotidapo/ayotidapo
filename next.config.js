898bec080bcabf09bcc054045cbc515c99b36bec
otidapson <oladapo.o@softcom.ng>
2021-09-03 13:38:15 +0100

    add connect info

--- next.config.js
+++ next.config.js
@@ -11,9 +11,9 @@ module.exports = (phase) => {
     };
   } else {
     env = {
-      mongodb_username: "ayotidapo_prod",
-      mongodb_password: "2YkcXq43KyPk0vqp",
-      mongodb_clustername: "cluster0",
+      mongodb_username: "",
+      mongodb_password: "",
+      mongodb_clustername: "",
       mongodb_database: "my_portfolio_prod",
     };
   }
