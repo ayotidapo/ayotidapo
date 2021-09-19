6744d78330c126e38c0c9e2d105a008861604fa5
otidapson <oladapo.o@softcom.ng>
2021-09-19 16:41:09 +0100

    update Resume

--- components/resume/index.js
+++ components/resume/index.js
@@ -10,7 +10,7 @@ const Resume = () => {
       {false && <Roller />}
       <div className={classes.iframe_div}>
         <iframe
-          src="https://res.cloudinary.com/oladapo/image/upload/v1632065505/OladapoResume_br2lar.pdf#toolbar=0&view=fitH&embedded=true"
+          src="https://res.cloudinary.com/oladapo/image/upload/v1632066032/OladapoResume_irdm6c.pdf#toolbar=0&view=fitH&embedded=true"
           allowtransparency="true"
           onLoad={() => setLoading(false)}
         />
