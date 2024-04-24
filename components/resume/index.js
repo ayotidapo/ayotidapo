c07b3a86d945183e279eaebc8702c7989cea02dc
ayotidapo <otidapson@gmail.com>
2024-04-24 18:01:50 +0100

    remove Unknown property 'allowtransparency'

--- components/resume/index.js
+++ components/resume/index.js
@@ -1,6 +1,6 @@
-import { useState } from "react";
-import Roller from "../Roller";
-import classes from "./resume.module.css";
+import { useState } from 'react';
+import Roller from '../Roller';
+import classes from './resume.module.css';
 
 const Resume = () => {
   const [loading, setLoading] = useState(true);
@@ -10,8 +10,7 @@ const Resume = () => {
       {false && <Roller />}
       <div className={classes.iframe_div}>
         <iframe
-          src="https://res.cloudinary.com/oladapo/image/upload/v1654627549/OladapoResume_portFolio_wwlz15.pdf#toolbar=0&view=fitH&embedded=true"
-          allowtransparency="true"
+          src='https://res.cloudinary.com/oladapo/image/upload/v1654627549/OladapoResume_portFolio_wwlz15.pdf#toolbar=0&view=fitH&embedded=true'
           onLoad={() => setLoading(false)}
         />
       </div>
